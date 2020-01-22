512f0f90953af4e9e3469677436fe8cc3be340ac
blakedave <blake_dave@hotmail.com>
2020-01-22 11:39:24 +0000

    removed old style float arrays

--- CashMachineLib/BaseAlgorithm.cs
+++ CashMachineLib/BaseAlgorithm.cs
@@ -20,10 +20,6 @@ namespace CashMachineLib
 
         public virtual void InitFloat()
         {
-            CashItems = new double[] { 50.00, 20.00, 10.00, 5.00, 2.00, 1.00, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01 };
-            CashItemsDisplay = new string[] { "£50", "£20", "£10", "£5", "£2", "£1", "50p", "20p", "10p", "5p", "2p", "1p" };
-            CashItemTotals = new int[] { 50, 50, 50, 50, 100, 100, 100, 100, 100, 100, 100, 100 };
-
             Float = new CashItem[]
             { 
                 new CashItem { Value=20.00, Display="£20", Total=50 },
@@ -43,13 +39,7 @@ namespace CashMachineLib
 
         protected CashItem[] Float { get; private set; }
 
-        protected double[] CashItems { get; set; }
-        
-        protected int[] CashItemTotals { get; set; }
-
-        protected string[] CashItemsDisplay { get; set; }
-
-        public virtual double Balance
+        public virtual double Balance 
         {
             get
             {
