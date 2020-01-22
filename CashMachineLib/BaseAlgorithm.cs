2b0c5ba946d9aae7286e24eb9cc0603c814ea8ae
blakedave <blake_dave@hotmail.com>
2020-01-22 11:24:55 +0000

    changed float to array of objects

--- CashMachineLib/BaseAlgorithm.cs
+++ CashMachineLib/BaseAlgorithm.cs
@@ -23,7 +23,25 @@ namespace CashMachineLib
             CashItems = new double[] { 50.00, 20.00, 10.00, 5.00, 2.00, 1.00, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01 };
             CashItemsDisplay = new string[] { "£50", "£20", "£10", "£5", "£2", "£1", "50p", "20p", "10p", "5p", "2p", "1p" };
             CashItemTotals = new int[] { 50, 50, 50, 50, 100, 100, 100, 100, 100, 100, 100, 100 };
+
+            Float = new CashItem[]
+            { 
+                new CashItem { Value=20.00, Display="£20", Total=50 },
+                new CashItem { Value=50.00, Display="£50", Total=50 },
+                new CashItem { Value=10.00, Display="£10", Total=50 },
+                new CashItem { Value=5.00, Display="£5", Total=50 },
+                new CashItem { Value=2.00, Display="£2", Total=100 },
+                new CashItem { Value=1.00, Display="£1", Total=100 },
+                new CashItem { Value=0.50, Display="50p", Total=100 },
+                new CashItem { Value=0.20, Display="20p", Total=100 },
+                new CashItem { Value=0.10, Display="10p", Total=100 },
+                new CashItem { Value=0.05, Display="5p", Total=100 },
+                new CashItem { Value=0.02, Display="2p", Total=100 },
+                new CashItem { Value=0.01, Display="1p", Total=100 }
+            };
         }
+
+        protected CashItem[] Float { get; private set; }
 
         protected double[] CashItems { get; set; }
         
@@ -37,9 +55,9 @@ namespace CashMachineLib
             {
                 double bal = 0.00;
 
-                for (int i = 0; i < CashItems.Length; i++)
+                for (int i = 0; i < Float.Length; i++)
                 {
-                    bal += CashItems[i] * CashItemTotals[i];
+                    bal += Float[i].Total * Float[i].Value;
                 }
                 return bal;
             }
---
+++ CashMachineLib/CashItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashMachineLib
+{
+    public class CashItem
+    {
+        public double Value { get; set; }
+        public int Total { get; set; }
+        public string Display { get; set; }
+    }
+}
